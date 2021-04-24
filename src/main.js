c9703ccbe63247d0b0bf3f4ff607fe55f7910b87
dvthanh0794 <dvthanh0794@gmail.com>
2021-04-24 15:56:55 +0700

    add authen

--- src/main.js
+++ src/main.js
@@ -4,9 +4,15 @@ import router from './router'
 import store from './store'
 import Antd from "ant-design-vue";
 import "ant-design-vue/dist/antd.css";
+import Axios from 'axios'
 
 Vue.use(Antd);
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.prototype.$http = Axios;
+const token = localStorage.getItem('token')
+if (token) {
+  Vue.prototype.$http.defaults.headers.common['Authorization'] = token
+}
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,78 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Router from 'vue-router'
+import store from '../store/index.js'
 import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import Login from '../components/Login.vue'
+import Secure from '../components/Secure.vue'
+import Register from '../components/Register.vue'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+// const routes = [
+//   {
+//     path: '/',
+//     name: 'Home',
+//     component: Home
+//   },
+//   {
+//     path: '/about',
+//     name: 'About',
+//     // route level code-splitting
+//     // this generates a separate chunk (about.[hash].js) for this route
+//     // which is lazy-loaded when the route is visited.
+//     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+//   }
+// ]
+
+let router = new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
+    },
+    {
+      path: '/secure',
+      name: 'secure',
+      component: Secure,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: About
+    }
+  ]
+})
+
+// const router = new VueRouter({
+//   routes
+// })
+
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(record => record.meta.requiresAuth)) {
+    if (store.getters.isLoggedIn) {
+      next()
+      return
+    }
+    next('/login')
+  } else {
+    next()
   }
-]
-
-const router = new VueRouter({
-  routes
 })
 
 export default router
