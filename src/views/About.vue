bca323f82e576d98335f0ced17b586f36b379f46
dvthanh0794 <dvthanh0794@gmail.com>
2021-04-24 13:02:53 +0700

    init

---
+++ src/views/About.vue
@@ -0,0 +1,26 @@
+<template>
+  <a-select
+    mode="multiple"
+    :default-value="['a1', 'b2']"
+    style="width: 100%"
+    placeholder="Please select"
+    @change="handleChange"
+  >
+    <a-select-option
+      v-for="i in 25"
+      :key="(i + 9).toString(36) + i"
+      :disabled="true"
+    >
+      {{ (i + 9).toString(36) + i }}
+    </a-select-option>
+  </a-select>
+</template>
+<script>
+export default {
+  methods: {
+    handleChange(value) {
+      console.log(`selected ${value}`);
+    },
+  },
+};
+</script>
